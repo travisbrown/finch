fca9a8eb67bc559c9ed08ad3b90e7f85ad891d72
Travis Brown <tbrown@twitter.com>
2015-07-17 12:11:55 -0500

    Try port 8123

--- petstore/src/it/scala/io/finch/petstore/PetstoreServiceIntegrationTest.scala
+++ petstore/src/it/scala/io/finch/petstore/PetstoreServiceIntegrationTest.scala
@@ -5,10 +5,16 @@ import org.scalatest.Matchers
 import org.scalatest.fixture.FlatSpec
 
 class PetstorePetServiceIntegrationTest extends FlatSpec with Matchers
- with ServiceIntegrationSuite with PetstorePetServiceSuite
+ with ServiceIntegrationSuite with PetstorePetServiceSuite {
+  override val port: Int = 8123
+}
 
 class PetstoreStoreServiceIntegrationTest extends FlatSpec with Matchers
- with ServiceIntegrationSuite with PetstoreStoreServiceSuite
+ with ServiceIntegrationSuite with PetstoreStoreServiceSuite {
+  override val port: Int = 8124
+}
 
 class PetstoreUserServiceIntegrationTest extends FlatSpec with Matchers
- with ServiceIntegrationSuite with PetstoreUserServiceSuite
+ with ServiceIntegrationSuite with PetstoreUserServiceSuite {
+  override val port: Int = 8125
+}
